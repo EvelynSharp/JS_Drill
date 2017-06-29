dc222d2465eb903af17aa06fba2b36c7f8b91655
Evelyn Sharp <yihongshi90@gmail.com>
2017-06-29 17:26:37 -0600

    practise

--- practice.js
+++ practice.js
@@ -19,3 +19,10 @@ function duckDuckGoose1(players, goose) {
 
 // Write a function f (in C# F) that returns Hello, world! string. But there is one thing, you cannot use helowrd letters.
 // The C# test cases ignore checking for banned letters in words class, string and public. Have fun!
+
+
+// c=()=> {
+//   return String.fromCharCode(72) + String.fromCharCode(69,76,76,79).toLowerCase()+','+String.fromCharCode(32,87,79,82,76,68).toLowerCase()+'!'
+// }
+
+so f=()=> ‘\x48\x65\x6C\x6C\x6F\x2C\x20\x77\x6F\x72\x6C\x64\x21’
