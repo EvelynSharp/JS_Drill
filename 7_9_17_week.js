2ca9e8cee942b37c59a5a104e91e8d3945ab1acd
Evelyn Sharp <yihongshi90@gmail.com>
2017-07-12 08:18:49 -0600

    6ku 7.12.17

--- 7_9_17_week.js
+++ 7_9_17_week.js
@@ -116,3 +116,56 @@ function expandedForm(num) {
   return resultArr.join(' + ');
 
 }
+
+
+// 7/12/17
+
+// Details
+// Solutions
+// Discourse (10)
+// Add to Collection|Share this kata:
+// A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward as forward. Examples of numerical palindromes are:
+//
+// 2332
+// 110011
+// 54322345
+//
+// For a given number num, return its closest numerical palindrome which can either be smaller or larger than num. If there are 2 possible values, the larger value should be returned. If num is a numerical palindrome itself, return it.
+//
+// For this kata, single digit numbers will NOT be considered numerical palindromes.
+//
+// Also, you know the drill - be sure to return "Not valid" if the input is not an integer or is less than 0.
+
+
+6 kyu
+Numerical Palindrome #4
+JavaScript:
+
+function palindrome(num) {
+  if( typeof num !== 'number' || num < 0) { return "Not valid" };
+  for( let i = 0; ; i++) {
+    let newnumArr1 = (num + i).toString().split('');
+    let newnumArr2 = (num - i).toString().split('');
+    let reverseArr1 = [];
+    let reverseArr2 = [];
+    for ( let x in newnumArr1) { reverseArr1.unshift(newnumArr1[x]) };
+    for ( let x in newnumArr2) { reverseArr2.unshift(newnumArr2[x]) };
+    if ( newnumArr1.join('') === reverseArr1.join('') && num+i >= 10) {
+      return (num + i)
+    }
+    if ( newnumArr2.join('') === reverseArr2.join('') && num-i >= 10) {
+      return ( num - i )
+    }
+  }  
+}
+
+function isPalindrome(num){
+  return (num>10 && num == num.toString().split("").reverse().join(""))? true : false;
+}
+function palindrome(num) {
+  if(!Number.isInteger(num) || num<0) return 'Not valid';
+  for(i=0; 1==1; i++){
+    if(isPalindrome(num+i)) return num+i;
+    if(isPalindrome(num-i)) return num-i;
+  }
+}
