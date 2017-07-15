d4d7e1a3b1c93547e6b5f90d8a641f3a7fcda9ec
Evelyn Sharp <yihongshi90@gmail.com>
2017-07-15 13:59:41 -0600

    6ku 7.15.17

--- 7_9_17_week.js
+++ 7_9_17_week.js
@@ -270,3 +270,21 @@ var coinCombo2 = function(cents) {
   })
   return result.reverse();
 }
+
+//
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+//
+// Note: If the number is a multiple of both 3 and 5, only count it once.
+
+function solution(number){
+  let sum = 0;
+  for ( let i = 1; i <= Math.floor(number/3); i++) {
+    if( 3*i < number ) { sum += ( 3*i )}
+  }
+  for ( let i = 1; i <= Math.floor(number/5); i++) {
+    if( i%3 !== 0 && 5*i < number ) { sum += ( 5*i )}
+  }
+  return sum;
+}
