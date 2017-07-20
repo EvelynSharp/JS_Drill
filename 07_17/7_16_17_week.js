62f5ffd503a0058ddb5ddce62bdd338eb9a16a36
Evelyn Sharp <yihongshi90@gmail.com>
2017-07-20 13:18:58 -0600

    6ku 7.20.17

--- 07_17/7_16_17_week.js
+++ 07_17/7_16_17_week.js
@@ -81,3 +81,23 @@ function isAlt(word) {
   }
   return true;
 }
+
+//7.20.17
+// Bob is preparing to pass IQ test. The most frequent task in this test is to find out which one of the given numbers differs from the others. Bob observed that one number usually differs from the others in evenness. Help Bob — to check his answers, he needs a program that among the given numbers finds one that is different in evenness, and return a position of this number.
+//
+// ! Keep in mind that your task is to help Bob solve a real IQ test, which means indexes of the elements start from 1 (not 0)
+
+function iqTest(numbers){
+  let numArr = numbers.split(' ').map( n => {return Number(n)} );
+  let result;
+  for ( let i = 1; i <numArr.length-1; i++) {
+    if (numArr[i]%2 !== numArr[i+1]%2 && numArr[i]%2 !== numArr[i-1]%2) {
+      return i+1
+    }
+  }
+  if (numArr[0]%2 !== numArr[1]%2 && numArr[0]%2 !== numArr[3]%2) {
+    return 1
+  } else {
+    return numArr.length;
+  }
+}
