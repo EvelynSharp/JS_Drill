948f204cb1c69d79e53b2f87fbd84e15ce31767c
Evelyn Sharp <yihongshi90@gmail.com>
2017-07-21 08:24:30 -0600

    6ku 7.21.17

--- 07_17/7_16_17_week.js
+++ 07_17/7_16_17_week.js
@@ -101,3 +101,46 @@ function iqTest(numbers){
     return numArr.length;
   }
 }
+
+// //7.21.17
+// You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+
+
+function isValidWalk(walk) {
+  if ( walk.length !== 10 ) { return false }
+  let sCount = walk.filter( n => n === 's').length;
+  let nCount = walk.filter( n => n === 'n').length;
+  let eCount = walk.filter( n => n === 'e').length;
+  let wCount = walk.filter( n => n === 'w').length;
+  if( sCount === nCount && eCount === wCount )
+    return true
+  return false
+}
+
+
+//clever
+function isValidWalk(walk) {
+  function count(val) {
+    return walk.filter(function(a){return a==val;}).length;
+  }
+  return walk.length==10 && count('n')==count('s') && count('w')==count('e');
+}
+
+//for performance, best practise - as only loop once
+
+function isValidWalk(walk) {
+  var dx = 0
+  var dy = 0
+  var dt = walk.length
+
+  for (var i = 0; i < walk.length; i++) {
+    switch (walk[i]) {
+      case 'n': dy--; break
+      case 's': dy++; break
+      case 'w': dx--; break
+      case 'e': dx++; break
+    }
+  }
+
+  return dt === 10 && dx === 0 && dy === 0
+}
