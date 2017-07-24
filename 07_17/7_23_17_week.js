6860edcdbac167964068adebc67408e53c61612a
Evelyn Sharp <yihongshi90@gmail.com>
2017-07-24 08:48:29 -0600

    6ku 7.24.17

--- 07_17/7_23_17_week.js
+++ 07_17/7_23_17_week.js
@@ -18,3 +18,27 @@ function deleteNth2(arr,x) {
     return cache[n] <= x;
   });
 }
+
+//7.24.17
+// The goal of this exercise is to convert a string to a new string where each character in the new string is '(' if that character appears only once in the original string, or ')' if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+function duplicateEncode(word){
+  let arr = word.toLowerCase().split('')
+  let newArr = arr.map( l => {
+    if ( arr.filter( n => n === l ).length > 1)
+      return ')'
+    return '('
+  })
+  return newArr.join('')
+}
+
+
+function 2(word){
+  return word
+    .toLowerCase()
+    .split('')
+    .map( function (a, i, w) {
+      return w.indexOf(a) == w.lastIndexOf(a) ? '(' : ')'
+    })
+    .join('');
+}
