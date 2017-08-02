f315872c083ebf9d2c395c84bcf52579374abc5a
Evelyn Sharp <yihongshi90@gmail.com>
2017-08-02 10:40:46 -0600

    6ku 8.2.17

--- 07_17/7_31_17_week.js
+++ 07_17/7_31_17_week.js
@@ -40,3 +40,27 @@ function divisors(integer) {
   }
   return result.length === 0 ? integer+' is prime' : result;
 };
+
+//8.2.17
+// You have an array of numbers.
+// Your task is to sort ascending odd numbers but even numbers must be on their places.
+//
+// Zero isn't an odd number and you don't need to move it. If you have an empty array, you need to return it.
+function sortArray(array) {
+  let oddArr = array.filter( n => n%2 !==0 ).sort( (a, b) => a-b);
+  let counter = -1;
+  return array.map( n => {
+    if( n%2 !== 0) {
+      counter++;
+      return oddArr[counter];
+    } else {
+      return n;
+    }
+  })
+}
+
+//shorter for using shift() instead of counter
+function sortArray(array) {
+  const odd = array.filter((x) => x % 2).sort((a,b) => a - b);
+  return array.map((x) => x % 2 ? odd.shift() : x);
+}
