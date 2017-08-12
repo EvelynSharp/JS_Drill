c124e384cea3dec3b3fcc2c54bdd21adfc78b5cb
Evelyn Sharp <yihongshi90@gmail.com>
2017-08-12 15:27:42 -0600

    6ku 8.12.17

--- Aug_2017.js
+++ Aug_2017.js
@@ -174,3 +174,22 @@ function checkReverse(arr) {
     return true
   return false
 }
+
+
+//8.12.17
+// Define a function isPrime that takes one integer argument and returns true or false depending on if the integer is a prime.
+//
+// Per Wikipedia, a prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+
+function isPrime(num) {
+  let r = true;
+  for( let i = 2; i <num; i++ ) {
+    if( num%i === 0) { r = false}
+  }
+  return num < 2 ? false : r;
+}
+
+function isPrime(num) {
+  for (var i = 2; i < num; i++) if (num % i == 0) return false;
+  return num >= 2;
+}
