76f93fab468b07e705e09365ded05ff38e0f3c12
Evelyn Sharp <yihongshi90@gmail.com>
2017-08-13 10:02:04 -0600

    6ku 8.13.17

--- Aug_2017.js
+++ Aug_2017.js
@@ -193,3 +193,18 @@ function isPrime(num) {
   for (var i = 2; i < num; i++) if (num % i == 0) return false;
   return num >= 2;
 }
+
+
+//8.13.17
+// Your task is to create a compose function to carry out this task, which will be passed two functions or lambdas. Ruby functions will be passed, and should return, either a proc or a lambda. Remember that the resulting composed function may be passed multiple arguments!
+function compose(f,g) {
+  return function(...a) {
+    return f( g(...a) )
+  }
+}
+
+function compose(f, g) {
+  return function() {
+    return f(g.apply(this, arguments));
+  };
+}
