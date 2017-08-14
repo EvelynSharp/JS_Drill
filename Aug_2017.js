62d568215ca142476c3d211b3e10a109757a7ce7
Evelyn Sharp <yihongshi90@gmail.com>
2017-08-14 13:20:11 -0600

    6ku 8.14.17

--- Aug_2017.js
+++ Aug_2017.js
@@ -208,3 +208,29 @@ function compose(f, g) {
     return f(g.apply(this, arguments));
   };
 }
+
+//8.14.17
+// You will be given a number and you will need to return it as a string in Expanded Form. For example:
+//
+// expandedForm(12); // Should return '10 + 2'
+// expandedForm(42); // Should return '40 + 2'
+// expandedForm(70304); // Should return '70000 + 300 + 4'
+function expandedForm(num) {
+ let arr = (''+num).split('');
+  let r = [];
+  for ( let i = 0; i < arr.length; i++ ) {
+    if(arr[i] !== '0' ) {
+      r.push( arr[i] + '0'.repeat(arr.length-1-i) )
+    }
+  }
+  return r.join(' + ')
+}
+//use filter and map instead of for loop
+
+const expandedForm2 = n => n.toString()
+                            .split("")
+                            .reverse()
+                            .map( (a, i) => a * Math.pow(10, i))
+                            .filter(a => a > 0)
+                            .reverse()
+                            .join(" + ");
