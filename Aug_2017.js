6a309f18612f40068e9fdf7c92564bb8d4b78294
Evelyn Sharp <yihongshi90@gmail.com>
2017-08-16 15:27:36 -0600

    6ku 8.16.17

--- Aug_2017.js
+++ Aug_2017.js
@@ -242,3 +242,36 @@ String.prototype.camelCase=function(){
   let arr = this.split(' ').map( s => s.charAt(0).toUpperCase() + s.slice(1) )
   return arr.join('')
 }
+
+
+//8.16.17
+//
+// Write a function that receives two strings and returns n, where n is equal to the number of characters we should shift the first string forward to match the second.
+//
+// For instance, take the strings "fatigue" and "tiguefa". In this case, the first string has been rotated 5 characters forward to produce the second string, so 5 would be returned.
+//
+// If the second string isn't a valid rotation of the first string, the method returns -1.
+// Examples:
+//
+// "coffee", "eecoff" => 2
+// "eecoff", "coffee" => 4
+// "moose", "Moose" => -1
+// "isn't", "'tisn" => 2
+// "Esham", "Esham" => 0
+// "dog", "god" => -1
+function shiftedDiff(first,second){
+  if ( first === second) { return 0 }
+  let r = -1;
+  let arr1 = first.split('')
+  for ( let i = 1; i < arr1.length; i++) {
+    arr1.unshift(arr1.pop())
+    if (arr1.join('') === second && r === -1) { r = i }
+  }
+  return r
+}
+
+
+function shiftedDiff2(first, second) {
+  if (first.length != second.length) return -1
+  return (second + second).indexOf(first)
+}
