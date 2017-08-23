6897d4f25d7ba9de3800ee803c93c95c0fe1eddf
Evelyn Sharp <yihongshi90@gmail.com>
2017-08-23 16:26:17 -0600

    6ku 8.23.17

--- Aug_2017.js
+++ Aug_2017.js
@@ -376,3 +376,17 @@ function once(fn) {
     }
   };
 }
+
+//8.23.17
+// Here's another staple for the functional programmer. You have a sequence of values and some predicate for those values. You want to get the longest prefix of elements such that the predicate is true for each element. We'll call this the takeWhile function. It accepts two arguments. The first is the sequence of values, and the second is the predicate function. The function does not change the value of the original sequence.
+
+function takeWhile (arr, pred) {
+  let arr1 = [], checker = true, i = 0;
+  while ( i < arr.length && checker) {
+    if ( pred( arr[i]) ) {
+      arr1.push( arr[i] )
+    } else {  checker = false }
+    i++
+  }
+  return arr1;
+}
