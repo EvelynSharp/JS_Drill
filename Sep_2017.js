9aee746c46956e1d356c389c9f757c66c5442719
Evelyn Sharp <yihongshi90@gmail.com>
2017-09-02 09:00:57 -0600

    5ku 9.2.17

--- Sep_2017.js
+++ Sep_2017.js
@@ -1,6 +1,6 @@
 //9.1.17
 //http://www.codewars.com/kata/is-my-friend-cheating/javascript
-//with time limite 
+//with time limite
 
 function removeNb (n) {
   let total = (n+1)*n/2
@@ -13,4 +13,28 @@ function removeNb (n) {
     }
   }
   return r;
+}
+
+
+//9.2.17
+//Maximum subarray sum
+var maxSequence = function(arr){
+  if(arr.length < 1) { return 0 }
+  let r = []
+  for ( let i = 1; i <= arr.length; i++) {
+    for ( let x = 0; x <= arr.length-i; x++ ) {
+      r.push(arr.slice(x,x+i).reduce( (t,n) => t += n, 0) )     
+    }
+  }
+  return Math.max(...r) >= 0 ? Math.max(...r) : 0
+}
+
+var maxSequence2 = function(arr){
+  var min = 0, ans = 0, i, sum = 0;
+  for (i = 0; i < arr.length; ++i) {
+    sum += arr[i];
+    min = Math.min(sum, min);
+    ans = Math.max(ans, sum - min);
+  }
+  return ans;
 }
