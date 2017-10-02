75dc4f0cd952191539f156d0665bf94cd851c0e8
Evelyn Sharp <yihongshi90@gmail.com>
2017-10-02 11:31:32 -0600

    6ku 10.2.17

---
+++ Oct_2017.js
@@ -0,0 +1,9 @@
+//10.2.17
+
+function count (string) {
+  const unique = [ ...new Set( string.split('') ) ]
+  let r = {}
+  unique.map( l => r[l] = 0 )
+  string.split('').map( l =>  r[l]++ )
+  return r
+}
