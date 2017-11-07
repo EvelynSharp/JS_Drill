0d5b9a48a7d6127b7409faaeeb2abdd099602cf7
Evelyn Sharp <yihongshi90@gmail.com>
2017-11-07 14:58:51 -0700

    5ku 11.7.17

--- Nov_2017.js
+++ Nov_2017.js
@@ -8,3 +8,32 @@ function firstNonRepeatingLetter(s) {
   })
   return result;
 }
+
+//11.7.17
+function anagrams(word, words) {
+  let result = [];
+  const wArr = word.split('');
+  const dirLength = Object.keys(makedir(word)).length;
+  words.map( w => {
+    const wDir = makedir(w);
+    let ifMatch = true;
+    for ( let props in wDir) {
+      if(wDir[props] !== wArr.filter( l => l === props ).length ) ifMatch = false;
+    }
+    if (ifMatch && Object.keys(wDir).length === dirLength ) result.push(w)
+  })
+  return result;
+}
+
+function makedir(str) {
+  let dir = {};
+  str.split('').map( l => {
+    if (!dir[l]) {
+      dir[l] = 1;
+    } else {
+      dir[l] += 1
+    }
+  })
+  return dir;
+}
+//should use sort , join, for exact match of strings
